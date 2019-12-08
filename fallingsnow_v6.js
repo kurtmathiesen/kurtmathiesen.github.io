b60126399eb4605719ca1d5452b3deab35ef4c3b
kurtmathiesen <31730499+kurtmathiesen@users.noreply.github.com>
2019-12-08 07:13:54 +0100

    Update fallingsnow_v6.js

--- fallingsnow_v6.js
+++ fallingsnow_v6.js
@@ -20,7 +20,7 @@ var browserWidth;
 var browserHeight;
 
 // Specify the number of snowflakes you want visible
-var numberOfSnowflakes = 2000;
+var numberOfSnowflakes = 1000;
 
 // Flag to reset the position of the snowflakes
 var resetPosition = false;
